efca525cce9aafa91d2823c64c1cc2e8da2f7f09
ellidaan <elone.meimoun@epitech.digital>
2023-03-21 14:55:37 +0100

    Update Homes.js

--- src/Components/Homes.js
+++ src/Components/Homes.js
@@ -21,7 +21,7 @@ const Homes = () => {
 
   return (
     <div className=" w-full  bg-red-300">
-      <h1>List of Pokemonnnnnn  </h1>
+      <h1>List of Pokemonnnnnnsqddsdddf  </h1>
 
       <input type="text" placeholder="Search Pokemon" value={searchQuery} onChange={e => setSearchQuery(e.target.value)} />
 
@@ -36,7 +36,9 @@ const Homes = () => {
             <div class="px-6 py-4 flex flex-col gap-4 justify-center items-center">
               <div class="font-bold text-xl mb-2">  <h2>{p.nom_pok}</h2></div>
               <img class="w-30 h-40 " src={p.image_pok} alt="pokemon" />
-             
+              {p.stats.map(stat => (
+                <li key={stat}>{stat.type1}/{stat.type2}</li>
+              ))}
 
             </div>
 
